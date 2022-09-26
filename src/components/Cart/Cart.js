273ce9ce2cbc410ade87a4361a41f48a9a06eea6
resalatislam5 <resalatislam5@gmail.com>
2022-09-26 13:16:51 +0600

    Order Summary

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Cart.css'
 const Cart = ({cart}) => {
     console.log(cart)
+    let price = 0;
+    let shipping = 0;
+    for (const product of cart) {
+        price = price + product.price;
+        shipping = shipping + product.shipping;
+    }
+    const tax =(price * 0.1).toFixed(2);
+    const total = price + shipping + parseFloat(tax);
     return (
         <div className='Cart-container'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: ${cart.price}</p>
-            <p>Total Shipping Charge: ${cart.length}</p>
-            <p>Tax: ${cart.length}</p>
-            <h6>Grand Total: ${cart.length}</h6>
+            <p>Total Price: ${price}</p>
+            <p>Total Shipping Charge: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h6>Grand Total: ${total}</h6>
             <div className='btn-container'>
                 <button className='btn-clear'>Clear Cart</button>
                 <button className='btn-review'>Review Order</button>
